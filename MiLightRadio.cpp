80d00083c8056de6fc099ea8ebc324e141b03f99
krulkip <krulkip@yahoo.com>
2017-04-16 12:14:45 +0200

    Add files via upload

---
+++ MiLightRadio.cpp
@@ -0,0 +1,179 @@
+/*
+ * MiLightRadio.cpp
+ *
+ *  Created on: 29 May 2015
+ *      Author: henryk
+ */
+
+#include "MiLightRadio.h"
+
+#define PACKET_ID(packet) ( ((packet[1] & 0xF0)<<24) | (packet[2]<<16) | (packet[3]<<8) | (packet[7]) )
+
+extern int mode;
+uint8_t CHANNELS[] = {0,0,0};
+
+MiLightRadio::MiLightRadio(AbstractPL1167 &pl1167)
+  : _pl1167(pl1167) {
+  _waiting = false;
+}
+
+int MiLightRadio::begin()
+{
+if (mode==1) {
+  uint8_t CHANNELS[] = {9, 40, 71};//default channels
+  #define RGBW
+}
+if (mode==2) {
+  uint8_t CHANNELS[] = {4, 39, 74};
+  #define CCT
+} 
+if (mode==3) {
+  uint8_t CHANNELS[] = {8, 39, 70};
+  #define RGB + CCT
+}
+if (mode==4) {
+  uint8_t CHANNELS[] = {3, 38, 73};
+  #define RGB
+}
+#define NUM_CHANNELS (sizeof(CHANNELS)/sizeof(CHANNELS[0]))
+  int retval = _pl1167.open();
+  if (retval < 0) {
+    return retval;
+  }
+
+  retval = _pl1167.setCRC(true);
+  if (retval < 0) {
+    return retval;
+  }
+
+  retval = _pl1167.setPreambleLength(3);
+  if (retval < 0) {
+    return retval;
+  }
+
+  retval = _pl1167.setTrailerLength(4);
+  if (retval < 0) {
+    return retval;
+  }
+#ifdef RGBW  
+  retval = _pl1167.setSyncword(0x147A, 0x258B);
+#endif
+#ifdef CCT
+  retval = _pl1167.setSyncword(0x050A, 0x55AA);
+#endif
+#ifdef RGBCCT
+  retval = _pl1167.setSyncword(0x7236, 0x1809);
+#endif  
+  if (retval < 0) {
+    return retval;
+  }  
+  retval = _pl1167.setMaxPacketLength(10);
+  if (retval < 0) {
+    return retval;
+  }
+
+  available();
+
+  return 0;
+}
+
+bool MiLightRadio::available()
+{
+  if (_waiting) {
+    return true;
+  }
+
+  if ((_pl1167.receive(CHANNELS[0]) > 0) || (_pl1167.receive(CHANNELS[1]) > 0) || (_pl1167.receive(CHANNELS[2]) > 0)){
+    size_t packet_length = sizeof(_packet);
+    if (_pl1167.readFIFO(_packet, packet_length) < 0) {
+      return false;
+    }
+    if (packet_length == 0 || packet_length != _packet[0] + 1U) {
+      return false;
+    }
+
+    uint32_t packet_id = PACKET_ID(_packet);
+    if (packet_id == _prev_packet_id) {
+      _dupes_received++;
+    } else {
+      _prev_packet_id = packet_id;
+      _waiting = true;
+    }
+  }
+  mode++;if (mode>3) mode=1;
+  if (mode==1){ // RGBW == default
+  _pl1167.setSyncword(0x147A, 0x258B);
+  CHANNELS[0]=9;CHANNELS[1]=40;CHANNELS[2]=71;
+  _pl1167.setMaxPacketLength(8);
+  _pl1167.recalc_parameters();
+  }
+  if (mode==2){ // CCT
+  _pl1167.setSyncword(0x050A, 0x55AA);
+  CHANNELS[0]=4;CHANNELS[1]=39;CHANNELS[1]=74;
+  _pl1167.setMaxPacketLength(8);
+  _pl1167.recalc_parameters();
+  }
+  if (mode==3){ // RGB + CCT
+  _pl1167.setSyncword(0x7236, 0x1809);
+  CHANNELS[0]=8;CHANNELS[1];39;CHANNELS[2]=70;
+  _pl1167.setMaxPacketLength(10);
+  _pl1167.recalc_parameters();
+  }
+  if (mode==4){ // RGB
+  _pl1167.setSyncword(0x9AAB, 0xBCCD);
+  CHANNELS[0]=3;CHANNELS[1]=38;CHANNELS[2]=73;
+  _pl1167.setMaxPacketLength(8);
+  _pl1167.recalc_parameters();
+  }
+  return _waiting;
+}
+
+int MiLightRadio::dupesReceived()
+{
+  return _dupes_received;
+}
+
+
+int MiLightRadio::read(uint8_t frame[], size_t &frame_length)
+{
+  if (!_waiting) {
+    frame_length = 0;
+    return -1;
+  }
+
+  if (frame_length > sizeof(_packet) - 1) {
+    frame_length = sizeof(_packet) - 1;
+  }
+
+  if (frame_length > _packet[0]) {
+    frame_length = _packet[0];
+  }
+
+  memcpy(frame, _packet + 1, frame_length);
+  _waiting = false;
+
+  return _packet[0];
+}
+
+int MiLightRadio::write(uint8_t frame[], size_t frame_length)
+{
+  if (frame_length > sizeof(_out_packet) - 1) {
+    return -1;
+  }
+  memcpy(_out_packet + 1, frame, frame_length);
+  _out_packet[0] = frame_length;
+  int retval = resend();
+  if (retval < 0) {
+    return retval;
+  }
+  return frame_length;
+}
+
+int MiLightRadio::resend()
+{
+  for (size_t i = 0; i < NUM_CHANNELS; i++) {
+    _pl1167.writeFIFO(_out_packet, _out_packet[0] + 1);
+    _pl1167.transmit(CHANNELS[i]);
+  }
+  return 0;
+}
---
+++ openmilibas027.ino
@@ -0,0 +1,112 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h> //http://tmrh20.github.io/RF24/
+#include "PL1167_nRF24.h"
+#include "MiLightRadio.h"
+
+// define connection pins for nRF24L01 shield on www.arduino-projects4u.com
+#define CE_PIN 9   //ESP8266 2
+#define CSN_PIN 10 //ESP8266 15
+
+#define V2_OFFSET(byte, key, jumpStart) ( \
+  V2_OFFSETS[byte-1][key%4] \
+    + \
+  ((jumpStart > 0 && key >= jumpStart && key <= jumpStart+0x80) ? 0x80 : 0) \
+) 
+int mode=1; // Transmitting mode 1 = RGBW   2 = CCT   3 = RGB + CCT   4 = RGB
+#define V2_OFFSET_JUMP_START 0x54
+
+uint8_t const V2_OFFSETS[][4] = {
+  { 0x45, 0x1F, 0x14, 0x5C },
+  { 0x2B, 0xC9, 0xE3, 0x11 },
+  { 0xEE, 0xDE, 0x0B, 0xAA },
+  { 0xAF, 0x03, 0x1D, 0xF3 },
+  { 0x1A, 0xE2, 0xF0, 0xD1 },
+  { 0x04, 0xD8, 0x71, 0x42 },
+  { 0xAF, 0x04, 0xDD, 0x07 },
+  { 0xE1, 0x93, 0xB8, 0xE4 }
+};
+uint8_t xorKey(uint8_t key) {
+  // Generate most significant nibble
+  const uint8_t shift = (key & 0x0F) < 0x04 ? 0 : 1;
+  const uint8_t x = (((key & 0xF0) >> 4) + shift + 6) % 8;
+  const uint8_t msn = (((4 + x) ^ 1) & 0x0F) << 4;
+  // Generate least significant nibble
+  const uint8_t lsn = ((((key & 0x0F) + 4)^2) & 0x0F);
+  return ( msn | lsn );
+}
+uint8_t decodeByte(uint8_t byte, uint8_t s1, uint8_t xorKey, uint8_t s2) {
+  uint8_t value = byte - s2;
+  value = value ^ xorKey;
+  value = value - s1;
+  return value;
+}
+uint8_t encodeByte(uint8_t byte, uint8_t s1, uint8_t xorKey, uint8_t s2) {
+  uint8_t value = byte + s1;
+  value = value ^ xorKey;
+  value = value + s2;
+  return value;
+}
+void decodeV2Packet(uint8_t *packet) {
+  uint8_t key = xorKey(packet[0]);
+  for (size_t i = 1; i <= 8; i++) {
+    packet[i] = decodeByte(packet[i], 0, key, V2_OFFSET(i, packet[0], V2_OFFSET_JUMP_START));
+  }
+}
+RF24 radio(CE_PIN, CSN_PIN);
+PL1167_nRF24 prf(radio);
+MiLightRadio mlr(prf);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  delay(1000);
+  Serial.println("# OpenMiLight Receiver/Transmitter starting");
+  mlr.begin();
+}
+
+static int dupesPrinted = 0;
+static enum {
+  IDLE,
+  HAVE_NIBBLE,
+  COMPLETE,
+} state;
+
+void loop()
+{
+    if (mlr.available()) {
+      uint8_t packet[9];
+      size_t packet_length = sizeof(packet);
+      Serial.println();
+      Serial.print("<-- ");
+      mlr.read(packet, packet_length);
+      if (packet_length<0x10) Serial.print("0");
+      Serial.print(packet_length,HEX);
+      Serial.print(" ");     
+      for (int i = 0; i < packet_length; i++) {
+        if (packet[i]<0x10) Serial.print("0");
+        Serial.print(packet[i],HEX);
+        Serial.print(" ");
+      }
+      if (packet_length!=7){
+      Serial.print("Decoded package = ");
+      uint8_t key = xorKey(packet[0]);
+      uint8_t sum = key;
+      Serial.print(key,HEX);Serial.print(" ");
+      for (size_t i = 1; i <= 7; i++) {
+        packet[i] = decodeByte(packet[i], 0, key, V2_OFFSET(i, packet[0], V2_OFFSET_JUMP_START));
+        sum += packet[i];
+        if (packet[i]<0x10) {Serial.print("0");}
+        Serial.print(packet[i],HEX);Serial.print(" ");
+      }
+    }
+    }
+    int dupesReceived = mlr.dupesReceived();
+    for (; dupesPrinted < dupesReceived; dupesPrinted++) {
+    Serial.print(".");
+    } 
+}
+
+
+
